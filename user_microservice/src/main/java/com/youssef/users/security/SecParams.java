554cd4dddd2d37f49b23f94effb9d5243d62fda2
YOUSEF BENARBIA <benarbiayousef@nabeul.r-iset.tn>
2024-10-09 06:40:36 +0100

    Update SecParams.java

--- user_microservice/src/main/java/com/youssef/users/security/SecParams.java
+++ user_microservice/src/main/java/com/youssef/users/security/SecParams.java
@@ -2,6 +2,6 @@ package com.youssef.users.security;
 
 public interface SecParams {
 	public static final long  EXP_TIME = 10*24*60*60*1000;
-	public static final String SECRET = "hacenaidi4455@gmail.com";
+	public static final String SECRET = "youssefbenarbia345@gmail.com";
 
 }

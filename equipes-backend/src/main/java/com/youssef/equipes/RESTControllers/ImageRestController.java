e77efcafc9876768d5fa8118ce1f7f7e9721b7ed
YOUSEF BENARBIA <benarbiayousef@nabeul.r-iset.tn>
2024-11-03 09:46:59 +0100

    Update ImageRestController.java

--- equipes-backend/src/main/java/com/youssef/equipes/RESTControllers/ImageRestController.java
+++ equipes-backend/src/main/java/com/youssef/equipes/RESTControllers/ImageRestController.java
@@ -1,57 +1,63 @@
 package com.youssef.equipes.RESTControllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.youssef.equipes.entities.Image;
+import com.youssef.equipes.services.ImageService;
+
+import java.io.IOException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.youssef.equipes.entities.Equipe;
-import com.youssef.equipes.services.EquipeService;
-
 @RestController
-@RequestMapping("/api")
-@CrossOrigin("*")
-public class EquipeRESTController {
-	
-	@Autowired
-	EquipeService equipeService;
-	
-	@RequestMapping(path="all",method = RequestMethod.GET)
-	public List<Equipe> getAllEquipes() {
-	return equipeService.getAllEquipes();
-	}
-	@RequestMapping(value="/getbyid/{id}",method = RequestMethod.GET)
-	public Equipe getEquipeById(@PathVariable("id") Long id) {
-	return equipeService.getEquipe(id);
-	}
-	@RequestMapping(path="/addeqip",method = RequestMethod.POST)
-	//@PreAuthorize("hasAuthority('ADMIN')")
-	public Equipe createEquipe(@RequestBody Equipe equipe) {
-	return equipeService.saveEquipe(equipe);
-	}
-	@RequestMapping(path="/updateeqip",method = RequestMethod.PUT)
-	public Equipe updateEquipe(@RequestBody Equipe equipe) {
-	return equipeService.updateEquipe(equipe);
-	}
-	
-	@RequestMapping(value="/deleqip/{id}",method = RequestMethod.DELETE)
-	public void deleteEquipe(@PathVariable("id") Long id)
-	{
-	equipeService.deleteEquipeById(id);
-	}
-	@RequestMapping(value="/equipeLig/{idLigue}",method = RequestMethod.GET)
-	public List<Equipe> getEquipesByCatId(@PathVariable("idLigue") Long idCat) {
-	return equipeService.findByLigueId(idCat);
-	}
-	
-	@RequestMapping(value="/eqipsByName/{nom}",method = RequestMethod.GET)
-	public List<Equipe> findByNomEquipeContains(@PathVariable("nom") String nom) {
-	return equipeService.findByNomEquipeContains(nom);
-	}
+@RequestMapping("/api/image")
+@CrossOrigin(origins = "*")
+public class ImageRestController {
+    @Autowired
+    ImageService imageService;
+
+    @GetMapping("/load/{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable("id") Long id) {
+        try {
+            return imageService.getImage(id);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body(null);
+        }
+    }
+
+    @GetMapping("/getImagesEqip/{idEqip}")
+    public List<Image> getImagesEqip(@PathVariable("idEqip") Long idEqip) throws IOException {
+        return imageService.getImagesParProd(idEqip);
+    }
+
+    @GetMapping("/get/info/{id}")
+    public Image getImageDetails(@PathVariable("id") Long id) throws IOException {
+        return imageService.getImageDetails(id);
+    }
+
+  
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteImage(@PathVariable("id") Long id) {
+        imageService.deleteImage(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/update")
+    public Image updateImage(@RequestParam("image") MultipartFile file) throws IOException {
+        return imageService.uploadImage(file);
+    }
+    
+    @PostMapping("/upload")
+    public ResponseEntity<Image> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
+        Image image = imageService.uploadImage(file);
+        return ResponseEntity.ok(image);
+    }
+
+    @PostMapping("/uploadImageEqip/{idEqip}")
+    public ResponseEntity<Image> uploadImageForEquipe(@RequestParam("image") MultipartFile file, @PathVariable("idEqip") Long idEqip) throws IOException {
+        Image image = imageService.uploadImageProd(file, idEqip);
+        return ResponseEntity.ok(image);
+    }
 }

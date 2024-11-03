6cbb5a3180587160a5d494458ed6d07f49014983
YOUSEF BENARBIA <benarbiayousef@nabeul.r-iset.tn>
2024-11-03 09:48:48 +0100

    Update ImageService.java

--- equipes-backend/src/main/java/com/youssef/equipes/services/ImageService.java
+++ equipes-backend/src/main/java/com/youssef/equipes/services/ImageService.java
@@ -9,11 +9,11 @@ import org.springframework.web.multipart.MultipartFile;
 import com.youssef.equipes.entities.Image;
 
 public interface ImageService {
-	Image uplaodImage(MultipartFile file) throws IOException;
-	Image getImageDetails(Long id) throws IOException;
-	ResponseEntity<byte[]> getImage(Long id) throws IOException;
-	void deleteImage(Long id) ;
-	
-	Image uplaodImageProd(MultipartFile file,Long idProd) throws IOException;
-	List<Image> getImagesParProd(Long prodId);
+    Image uploadImage(MultipartFile file) throws IOException;
+    Image getImageDetails(Long id) throws IOException;
+    ResponseEntity<byte[]> getImage(Long id) throws IOException;
+    void deleteImage(Long id);
+    
+    Image uploadImageProd(MultipartFile file, Long idProd) throws IOException;
+    List<Image> getImagesParProd(Long prodId);
 }

3282178b5d4cb7543d0fe231c9d0e7976397bb73
YOUSEF BENARBIA <benarbiayousef@nabeul.r-iset.tn>
2024-11-03 09:49:01 +0100

    Update ImageServiceImpl.java

--- equipes-backend/src/main/java/com/youssef/equipes/services/ImageServiceImpl.java
+++ equipes-backend/src/main/java/com/youssef/equipes/services/ImageServiceImpl.java
@@ -3,6 +3,7 @@ package com.youssef.equipes.services;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,54 +17,75 @@ import com.youssef.equipes.repo.ImageRepository;
 
 @Service
 public class ImageServiceImpl implements ImageService {
-	@Autowired
-	ImageRepository imageRepository;
-	@Autowired
-	EquipeService equipeService;
-	@Autowired
-	EquipeRepository equipeRepository;
-	@Override
-	public Image uplaodImage(MultipartFile file) throws IOException {
-		/*
-		 * Ce code commenté est équivalent au code utilisant le design pattern Builder
-		 * Image image = new Image(null, file.getOriginalFilename(),
-		 * file.getContentType(), file.getBytes(), null); return
-		 * imageRepository.save(image);
-		 */
-		return imageRepository.save(Image.builder().name(file.getOriginalFilename()).type(file.getContentType())
-				.image(file.getBytes()).build());
-	}
+    @Autowired
+    ImageRepository imageRepository;
+    @Autowired
+    EquipeService equipeService;
+    @Autowired
+    EquipeRepository equipeRepository;
+    
+    @Override
+    public Image uploadImage(MultipartFile file) throws IOException {
+        return imageRepository.save(Image.builder()
+            .name(file.getOriginalFilename())
+            .type(file.getContentType())
+            .image(file.getBytes())
+            .build());
+    }
 
-	@Override
-	public Image getImageDetails(Long id) throws IOException {
-		final Optional<Image> dbImage = imageRepository.findById(id);
-		return Image.builder().idImage(dbImage.get().getIdImage()).name(dbImage.get().getName())
-				.type(dbImage.get().getType()).image(dbImage.get().getImage()).build();
-	}
+    @Override
+    public Image getImageDetails(Long id) throws IOException {
+        final Optional<Image> dbImage = imageRepository.findById(id);
+        if (dbImage.isPresent()) {
+            return Image.builder()
+                .idImage(dbImage.get().getIdImage())
+                .name(dbImage.get().getName())
+                .type(dbImage.get().getType())
+                .image(dbImage.get().getImage())
+                .build();
+        } else {
+            throw new IOException("Image not found");
+        }
+    }
 
-	@Override
-	public ResponseEntity<byte[]> getImage(Long id) throws IOException {
-		final Optional<Image> dbImage = imageRepository.findById(id);
-		return ResponseEntity.ok().contentType(MediaType.valueOf(dbImage.get().getType()))
-				.body(dbImage.get().getImage());
-	}
+    @Override
+    public ResponseEntity<byte[]> getImage(Long id) throws IOException {
+        final Optional<Image> dbImage = imageRepository.findById(id);
+        if (dbImage.isPresent()) {
+            return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(dbImage.get().getType()))
+                .body(dbImage.get().getImage());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-	@Override
-	public void deleteImage(Long id) {
-		imageRepository.deleteById(id);
-	}
+    @Override
+    public void deleteImage(Long id) {
+        imageRepository.deleteById(id);
+    }
 
-	@Override
-	public Image uplaodImageProd(MultipartFile file, Long idProd) throws IOException {
-		Equipe p = new Equipe();
-		p.setIdEquipe(idProd);
-		return imageRepository.save(Image.builder().name(file.getOriginalFilename()).type(file.getContentType())
-				.image(file.getBytes()).equipe(p).build());
-	}
 
-	@Override
-	public List<Image> getImagesParProd(Long equipeId) {
-		Equipe p = equipeRepository.findById(equipeId).get();
-		return p.getImages();
-	}
+    @Override
+    public Image uploadImageProd(MultipartFile file, Long idProd) throws IOException {
+        Equipe equipe = equipeRepository.findById(idProd).orElseThrow(() -> new IOException("Equipe not found"));
+        Image image = Image.builder()
+            .name(file.getOriginalFilename())
+            .type(file.getContentType())
+            .image(file.getBytes())
+            .equipe(equipe)
+            .build();
+        image = imageRepository.save(image);
+
+        equipe.addImage(image); // Add the image to the equipe
+        equipeRepository.save(equipe); // Save the equipe with the new image
+
+        return image;
+    }
+
+    @Override
+    public List<Image> getImagesParProd(Long equipeId) {
+        Equipe p = equipeRepository.findById(equipeId).orElseThrow(() -> new IllegalArgumentException("Equipe not found"));
+        return p.getImages();
+    }
 }
